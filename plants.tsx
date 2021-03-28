a43488dac256ab11bed7829f53be113b14c77bf7
DrJamgo <drjamgo@hotmail.com>
2021-03-28 09:56:26 +0200

    FEAT: Add tsx files for Interior and others

---
+++ plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="plants" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="plants.png" width="512" height="1024"/>
+</tileset>
